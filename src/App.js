073c611cc04537163029ffd8c2ca87a8ac4af17f
mani <mani.jain@orangetree.software>
2022-06-12 19:58:59 +0530

    Changes have been made in App component and added PurpleButton Component.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from 'react'
+import YellowButton from "./YellowButton"
+import BlueButton from "./BlueButton"
+import PurpleButton from './PurpleButton'
 
 export default function App() {
   return (
-    <div>App</div>
+    <div>
+      <h1>The App Component</h1>
+
+      <YellowButton />
+
+      <BlueButton />
+
+      <PurpleButton />
+
+    </div>
   )
 }
---
+++ src/PurpleButton.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function PurpleButton() {
+  return (
+    <div>PurpleButton</div>
+  )
+}
